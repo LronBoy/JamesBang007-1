d9a7836dd71044fdbfecedf7eab43b93520bff96
James邦 <jmshbang@gmail.com>
2020-01-20 17:54:16 +0800

    es6 Module的语法

---
+++ 练习/es6/main.js
@@ -0,0 +1,6 @@
+import { mianji, zhouchang, year } from './yuan.js'
+
+
+console.log(mianji(10));
+console.log(zhouchang(10));
+console.log(year);
---
+++ 练习/es6/yuan.js
@@ -0,0 +1,13 @@
+export const mianji = function(r) {
+    return 3.14 * r * r;
+}
+
+export function zhouchang(r) {
+    return 2 * 3.14 * r
+}
+
+export var year = 1958;
+
+// 报错（写法通过变量m，还是直接输出 1。1只是一个值，不是接口。）
+// var m = 1;
+// export m;
